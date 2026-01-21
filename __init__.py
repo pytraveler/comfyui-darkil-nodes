5c54462584ebc0b22889d593808e10bca4fd3fe7
pytraveler <travelerpy99@gmail.com>
2026-01-21 22:13:28 +0300

    project init

---
+++ __init__.py
@@ -0,0 +1,15 @@
+# imports here...
+
+
+NODE_CLASS_MAPPINGS = {
+}
+
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+}
+
+
+WEB_DIRECTORY = "./web/js"
+
+
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
